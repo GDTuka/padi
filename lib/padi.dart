75b74e18e0d5c90aeeb3e8cd3f3e4667eb952c05
artem soglaev <artem.soglaev@mail.ru>
2025-02-07 21:05:09 +0300

    new version on inherited widget

--- lib/padi.dart
+++ lib/padi.dart
@@ -7,9 +7,11 @@ import 'package:provider/provider.dart';
 /// DI container
 abstract class Padi extends IPadi {
   final Completer<void> _inited = Completer();
+
   @override
-  void init(BuildContext context) {
-    initAsync(context).then((value) => _inited.complete()).onError((e, st) => onError(context, e, st));
+  Future<void> init(BuildContext context) async {
+    // ignore: use_build_context_synchronously
+    await initAsync(context).then((value) => _inited.complete()).onError((e, st) => onError(context, e, st));
   }
 
   Future<void> initAsync(BuildContext context);
@@ -23,16 +25,15 @@ abstract class Padi extends IPadi {
 
 /// interface for DI container
 abstract class IPadi extends ChangeNotifier {
-  void init(BuildContext context);
+  Future<void> init(BuildContext context);
 
   static T of<T extends IPadi>(BuildContext context) => context.read<T>();
 
   static T depend<T extends IPadi>(BuildContext context) => context.watch<T>();
 }
 
-/// Padi widget
-class PadiWidget<T extends Padi> extends ChangeNotifierProvider<T> {
-  PadiWidget({
+class OldPadiWidget<T extends Padi> extends ChangeNotifierProvider<T> {
+  OldPadiWidget({
     super.key,
     required T Function() create,
     required WidgetBuilder loaderBuilder,
@@ -55,3 +56,70 @@ class PadiWidget<T extends Padi> extends ChangeNotifierProvider<T> {
         );
 }
 
+class _PadiScope<T extends IPadi> extends InheritedWidget {
+  const _PadiScope({
+    super.key,
+    required this.padi,
+    required super.child,
+  });
+
+  final IPadi padi;
+
+  // ignore: unused_element
+  static T? maybeOf<T extends IPadi>(BuildContext context) =>
+      context.dependOnInheritedWidgetOfExactType<_PadiScope<T>>()! as T;
+
+  // ignore: unused_element
+  static T of<T extends IPadi>(BuildContext context) {
+    final padi = maybeOf<T>(context);
+    if (padi == null) {
+      throw StateError('No Padi found in context');
+    }
+    return padi;
+  }
+
+  @override
+  bool updateShouldNotify(covariant InheritedWidget oldWidget) {
+    return false;
+  }
+}
+
+class PadiWidget<T extends IPadi> extends StatelessWidget {
+  const PadiWidget({
+    super.key,
+    required this.create,
+    required this.child,
+    required this.loaderBuilder,
+    this.errorBuilder,
+  });
+
+  final Widget child;
+  final WidgetBuilder loaderBuilder;
+  final WidgetBuilder? errorBuilder;
+  final T Function() create;
+
+  Future<T> _init(BuildContext context) async {
+    final padi = create();
+    await padi.init(context);
+    return padi;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<void>(
+      future: _init(context),
+      builder: (context, snap) {
+        if (snap.connectionState != ConnectionState.done) {
+          return loaderBuilder(context);
+        }
+        if (snap.hasError && errorBuilder != null) {
+          return errorBuilder!(context);
+        }
+        return _PadiScope<T>(
+          padi: snap.data as T,
+          child: child,
+        );
+      },
+    );
+  }
+}
